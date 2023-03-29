540fe55bf815bb0d60462d9dbe22727d9503efab
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-29 18:23:16 +0300

    Create Software Developer repository

---
+++ src/main/java/com/alpdogan/CompanyManagementSystem/repository/SoftwareDeveloperRepository.java
@@ -0,0 +1,9 @@
+package com.alpdogan.CompanyManagementSystem.repository;
+
+import com.alpdogan.CompanyManagementSystem.entity.SoftwareDeveloper;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SoftwareDeveloperRepository extends CrudRepository<SoftwareDeveloper, Integer> {
+}
