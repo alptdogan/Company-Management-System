ce8f0a76d413c71e34380549658c30974a352f00
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-29 18:40:59 +0300

    Create DTOs for Tech Consultant entity

---
+++ src/main/java/com/alpdogan/CompanyManagementSystem/dto/request/SaveTechConsultantRequestDto.java
@@ -0,0 +1,10 @@
+package com.alpdogan.CompanyManagementSystem.dto.request;
+
+import lombok.Data;
+
+@Data
+public class SaveTechConsultantRequestDto {
+
+    private String fullName;
+
+}
---
+++ src/main/java/com/alpdogan/CompanyManagementSystem/dto/response/TechConsultantResponseDto.java
@@ -0,0 +1,14 @@
+package com.alpdogan.CompanyManagementSystem.dto.response;
+
+import lombok.Data;
+
+@Data
+public class TechConsultantResponseDto {
+
+    private int id;
+
+    private String fullName;
+
+    private int techCrewId;
+
+}
