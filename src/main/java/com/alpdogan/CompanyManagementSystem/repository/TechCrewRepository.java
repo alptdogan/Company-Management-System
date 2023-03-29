81bf4b0c4dafe565d5387d43dd510fabebd60ccf
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-29 18:21:45 +0300

    Create TechCrew repository

---
+++ src/main/java/com/alpdogan/CompanyManagementSystem/repository/TechCrewRepository.java
@@ -0,0 +1,10 @@
+package com.alpdogan.CompanyManagementSystem.repository;
+
+import com.alpdogan.CompanyManagementSystem.entity.TechCrew;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TechCrewRepository extends CrudRepository<TechCrew, Integer> {
+
+}
