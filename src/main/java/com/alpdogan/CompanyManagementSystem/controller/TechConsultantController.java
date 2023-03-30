db6863106fbcbe2360d8ec8c11485266e2aec513
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-30 15:30:17 +0300

    Create TechConsultantController

---
+++ src/main/java/com/alpdogan/CompanyManagementSystem/controller/TechConsultantController.java
@@ -0,0 +1,67 @@
+package com.alpdogan.CompanyManagementSystem.controller;
+
+import com.alpdogan.CompanyManagementSystem.dto.request.SaveTechConsultantRequestDto;
+import com.alpdogan.CompanyManagementSystem.dto.request.UpdateTechConsultantRequestDto;
+import com.alpdogan.CompanyManagementSystem.dto.response.TechConsultantResponseDto;
+import com.alpdogan.CompanyManagementSystem.entity.TechConsultant;
+import com.alpdogan.CompanyManagementSystem.service.TechConsultantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/techConsultants")
+public class TechConsultantController {
+
+    @Autowired
+    TechConsultantService techConsultantService;
+
+    @GetMapping("/findTechConsultantById")
+    public ResponseEntity<TechConsultant> findTechConsultantById (@RequestParam int techConsultantId) {
+
+        TechConsultant techConsultant = techConsultantService.findTechConsultantById(techConsultantId);
+
+        return new ResponseEntity<>(techConsultant, HttpStatus.OK);
+
+    }
+
+    @GetMapping("findAllTechConsultants")
+    public ResponseEntity<List<TechConsultantResponseDto>> findAllTechConsultants() {
+
+        List<TechConsultantResponseDto> techConsultantResponseDtos = techConsultantService.findAllTechConsultants();
+
+        return new ResponseEntity<>(techConsultantResponseDtos, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/saveTechConsultant")
+    public ResponseEntity<String> saveTechConsultant (@RequestBody SaveTechConsultantRequestDto saveTechConsultantRequestDto) {
+
+        String techConsultantSaveDescription = techConsultantService.saveTechConsultant(saveTechConsultantRequestDto);
+
+        return new ResponseEntity<>(techConsultantSaveDescription, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/updateTechConsultant")
+    public ResponseEntity<String> updateTechConsultantById (@RequestBody UpdateTechConsultantRequestDto updateTechConsultantRequestDto) {
+
+        String updateTechConsultantDescription = techConsultantService.updateTechConsultantById(updateTechConsultantRequestDto);
+
+        return new ResponseEntity<>(updateTechConsultantDescription, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/deleteTechConsultant")
+    public ResponseEntity<String> deleteTechConsultantById(@RequestParam Integer techConsultantId) {
+
+        String deleteTechConsultantDescription = techConsultantService.deleteTechConsultantById(techConsultantId);
+
+        return new ResponseEntity<>(deleteTechConsultantDescription, HttpStatus.OK);
+
+    }
+
+}
